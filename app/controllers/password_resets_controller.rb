8632d7bcb33016b151c0f31761d7c4bf411cf95d
Nate <npauzenga@gmail.com>
2015-12-23 20:12:03 -0500

    start password_resets_controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,2 +1,21 @@
 class PasswordResetsController < ApplicationController
+  def create
+    reset = PasswordResetOrganizer.call(email: password_params[:password_reset][:email])
+    if reset.success?
+      render json: { password: reset.message }, status: :created
+      redirect_to root_url
+    else
+      render json: reset.errors, status: :not_found
+      redirect_to root_url
+    end
+  end
+
+  def update
+  end
+
+  private
+
+  def password_params
+    params.require(:password_reset).permit(:email)
+  end
 end
