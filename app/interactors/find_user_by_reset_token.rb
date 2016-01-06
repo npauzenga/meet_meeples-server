e2c1c09955cd33601e9485121f07a2e28060d6ca
Nate <npauzenga@gmail.com>
2016-01-05 22:18:00 -0500

    FindUserByResetToken red specs

--- app/interactors/find_user_by_reset_token.rb
+++ app/interactors/find_user_by_reset_token.rb
@@ -1,6 +1,12 @@
 class FindUserByResetToken
   include Interactor
 
-  def call
+  def validate_input
+  end
+
+  def execute
+  end
+
+  def validate_output
   end
 end
---
+++ spec/interactors/find_user_by_reset_token_spec.rb
@@ -0,0 +1,59 @@
+RSpec.describe FindUserByResetToken do
+  describe ".call" do
+    let(:token) { Encryptor.generate_token }
+
+    context "when successful" do
+      before do
+        create(:confirmed_user, reset_digest: token[0])
+      end
+
+      subject do
+        described_class.call(reset_token: token[1])
+      end
+
+      it "is a success" do
+        is_expected.to be_a_success
+      end
+
+      it "find the user" do
+        expect(subject.user).to eq(user)
+      end
+    end
+
+    context "when reset token not provided" do
+      before do
+        create(:confirmed_user, reset_digest: token[0])
+      end
+
+      subject do
+        described_class.call(reset_token: nil)
+      end
+
+      it "fails" do
+        is_expected.to be_a_failure
+      end
+
+      it "adds an error to errors" do
+        expect(subject.errors).to eq("invalid input")
+      end
+    end
+
+    context "when user not found" do
+      before do
+        create(:confirmed_user, reset_digest: "differenttoken")
+      end
+
+      subject do
+        described_class.call(reset_token: token[1])
+      end
+
+      it "fails" do
+        is_expected.to be_a_failure
+      end
+
+      it "adds an error to errors" do
+        expect(subject.errors).to eq("invalid output")
+      end
+    end
+  end
+end
