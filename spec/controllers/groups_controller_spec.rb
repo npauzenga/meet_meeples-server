248f9b39b7a5e3836ab1a8f178c5ca5a4367dbf6
Nate <npauzenga@gmail.com>
2016-01-08 10:50:05 -0500

    add serialization to #create specs and start #show specs

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -2,6 +2,15 @@ include Helpers
 
 RSpec.describe GroupsController do
   let(:group) { create(:group) }
+  let(:serializer) { GroupSerializer.new(group) }
+
+  let(:serialization) do
+    ActiveModel::Serializer::Adapter.create(serializer)
+  end
+
+  before do
+    authenticate
+  end
 
   describe "POST #create" do
     let(:params) { { group: create_group_input.fetch(:group_params) } }
@@ -65,24 +74,47 @@ RSpec.describe GroupsController do
   end
 
   describe "GET #show" do
+    let(:group) { create(:group) }
+    let(:params) { { id: group.id } }
+    let(:show_group_input) { { id: params.fetch(:id).to_s } }
+
+    let(:show_group_context) do
+      Interactor::Context.new(errors: :val, group: group)
+    end
+
+    before do
+      allow(ShowGroup).to receive(:call).with(show_group_input).
+        and_return(show_group_context)
+    end
+
     context "when called" do
       it "calls the ShowGroup Interactor" do
+        expect(ShowGroup).to receive(:call)
+        get :show, params
       end
     end
 
     context "when ShowGroup is successful" do
       it "returns HTTP status 201" do
+        get :show, params
+        expect(response).to have_http_status(200)
       end
 
       it "renders the group as JSON" do
+        get :show, params
+        expect(serialization.to_json).to eq(response.body)
       end
     end
 
     context "when ShowGroup fails" do
-      it "returns HTTP status xxx" do
+      it "returns HTTP status 404" do
+        get :show, params
+        expect(response).to have_http_status(404)
       end
 
       it "renders an error" do
+        get :show, params
+        expect(response.body).to eq("invalid")
       end
     end
   end
