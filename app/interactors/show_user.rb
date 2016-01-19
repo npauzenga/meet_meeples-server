ff0c1966629714401b1fe34eb755ffe7c1035def
Nate <npauzenga@gmail.com>
2016-01-18 21:26:01 -0500

    remove ShowUser rescue
    I think it’s better to not rescue this exception. We’ll have to handle
    RecordNotFound.

--- app/interactors/show_user.rb
+++ app/interactors/show_user.rb
@@ -5,11 +5,5 @@ class ShowUser < StandardInteraction
 
   def execute
     context.user = User.find(context.id)
-  rescue ActiveRecord::RecordNotFound
-    context.user = nil
-  end
-
-  def validate_output
-    context.fail!(error: "invalid user") unless context.user
   end
 end
