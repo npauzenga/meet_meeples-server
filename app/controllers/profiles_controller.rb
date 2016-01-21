f9b2e721bfa8136b9bcfce0d19860ddab1a858f9
Nate <npauzenga@gmail.com>
2016-01-20 19:53:00 -0500

    add IndexProfile interactor and profiles controller spec

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,15 +2,22 @@ class ProfilesController < AuthenticationController
   # GET /profiles/1
   def show
     result = ShowProfile.call(id: params[:id])
+
     if result.success?
       render json: result.user, status: :ok
     else
-      render json: result.error, status: :not_found
+      render json: result.errors, status: :not_found
     end
   end
 
   # GET /profiles
   def index
-    
+    result = IndexProfile.call
+
+    if result.success?
+      render json: result.profiles, status: :ok
+    else
+      render json: result.errors, status: :internal_server_error
+    end
   end
 end
--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe ProfilesController do
 
     context "when ShowProfile fails" do
       let(:show_profile_context) do
-        double(:context, error: "invalid", success?: false)
+        double(:context, errors: "invalid", success?: false)
       end
 
       it "returns HTTP status 404" do
@@ -47,6 +47,59 @@ RSpec.describe ProfilesController do
       it "renders an error" do
         get :show, params
         expect(response.body).to eq("invalid")
+      end
+    end
+  end
+
+  describe "GET #index" do
+    let(:user)  { create(:confirmed_user) }
+    let(:user2) { create(:confirmed_user) }
+    let(:user3) { create(:confirmed_user) }
+
+    let(:index_profile_context) do
+      Interactor::Context.new(errors:   :val,
+                              profiles: [user, user2, user3])
+    end
+
+    before(:example) do
+      allow(IndexProfile).to receive(:call).and_return(index_profile_context)
+    end
+
+    before do
+      authenticate
+    end
+
+    context "when succesful" do
+      it "calls the IndexProfile interactor" do
+        expect(IndexProfile).to receive(:call)
+        get :index
+      end
+
+      it "returns HTTP status 200" do
+        get :index
+        expect(response).to have_http_status(200)
+      end
+
+      it "render the profiles as JSON" do
+        get :index
+        expect(response.body).
+          to eq([serialize(user), serialize(user2), serialize(user3)])
+      end
+    end
+
+    context "when IndexProfile fails" do
+      let(:index_profile_context) do
+        double(:context, errors: "internal server error", success?: false)
+      end
+
+      it "returns HTTP status 500" do
+        get :index
+        expect(response).to have_http_status(500)
+      end
+
+      it "renders an error" do
+        get :index
+        expect(response.body).to eq("internal server error")
       end
     end
   end
