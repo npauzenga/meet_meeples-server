62066fa4a3e618218cac1ad67a8d7d7f9c1df3ae
Nate <npauzenga@gmail.com>
2016-01-23 17:34:24 -0500

    add rolify and delete deprecated attributes from Group and GameNight

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,10 +1,10 @@
 class Group < ActiveRecord::Base
+  resourcify
+
   has_many :user_group_memberships
   has_many :users, through: :user_group_memberships
 
   has_many :game_nights
-
-  belongs_to :moderator, class_name: :User, foreign_key: :moderator_id
 
   validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110154158) do
+ActiveRecord::Schema.define(version: 20160123223328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20160110154158) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.integer  "group_id"
-    t.integer  "organizer_id"
     t.string   "name"
   end
 
@@ -36,10 +35,20 @@ ActiveRecord::Schema.define(version: 20160110154158) do
     t.string   "country"
     t.string   "facebook"
     t.string   "twitter"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.integer  "moderator_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "resource_id"
+    t.string   "resource_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
+  add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
 
   create_table "user_game_night_attendees", force: :cascade do |t|
     t.integer  "user_id"
@@ -76,6 +85,13 @@ ActiveRecord::Schema.define(version: 20160110154158) do
     t.string   "state"
     t.string   "country"
   end
+
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+  end
+
+  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
 
   add_foreign_key "game_nights", "groups"
   add_foreign_key "user_game_night_attendees", "game_nights"
