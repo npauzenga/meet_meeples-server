d7dae23fb573b8b4f20051d30205487c2e68de25
Nate <npauzenga@gmail.com>
2016-01-25 18:05:17 -0500

    new error scheme for CreateUser and Interactors generally

--- lib/standard_interaction.rb
+++ lib/standard_interaction.rb
@@ -10,9 +10,14 @@ class StandardInteraction
   def execute
   end
 
-  def validate_input
+  def validate_input(input: expected_arguments)
+    context.fail!(errors: input_error) unless context.send(input)
   end
 
   def validate_output
+  end
+
+  def input_error
+    { "#{self.class.name}": "invalid Interactor input" }
   end
 end
