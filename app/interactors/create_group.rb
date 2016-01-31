088c6ef0889535c6196d3acf728aef857114af1e
Nate <npauzenga@gmail.com>
2016-01-30 20:13:01 -0500

    CreateGroup now validates all input correctly

--- app/interactors/create_group.rb
+++ app/interactors/create_group.rb
@@ -1,6 +1,6 @@
 class CreateGroup < StandardInteraction
   def validate_input
-    context.fail!(errors: "invalid input") unless context.group_params
+    context.fail!(errors: "invalid input") unless valid_input?
   end
 
   def execute
@@ -9,5 +9,11 @@ class CreateGroup < StandardInteraction
 
   def validate_output
     context.fail!(errors: context.group.errors) unless context.group.persisted?
+  end
+
+  private
+
+  def valid_input?
+    context.group_params && context.user
   end
 end
